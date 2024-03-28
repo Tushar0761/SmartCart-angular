28c82de39fb4a8ab97824ff80218064f5010fd5f
Tushar0761 <tusharpanchal0761@gmail.com>
2024-03-28 18:45:32 +0530

    day 4 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { CartComponent } from './component/cart/cart.component';
+import { OrderComponent } from './component/order/order.component';
 
 const routes: Routes = [
-   { path: '', component: ProductListComponent }, 
-   { path: 'login', component: LoginComponent },
+  { path: '', component: ProductListComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductListComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'order', component: OrderComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-
 import { ProductViewComponent } from './component/product-view/product-view.component';
-import { ProductListComponent } from './component/product-list/product-list.component'
+import { ProductListComponent } from './component/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,21 +12,27 @@ import { PageNotFoundComponent } from './component/page-not-found/page-not-found
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { CartComponent } from './component/cart/cart.component';
+import { OrderComponent } from './component/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-     ProductListComponent,
-      ProductViewComponent, 
-      PageNotFoundComponent, NavbarComponent, FooterComponent
+    ProductListComponent,
+    ProductViewComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CartComponent,
+    OrderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
